2885f820dd3720030ddc26496d4dbd42bccd088b
Ryan J. Geyer <me@ryangeyer.com>
2014-04-03 17:49:44 -0700

    Have to set node attributes at compile time since they're evaulated at compile time

--- recipes/deserialize.rb
+++ recipes/deserialize.rb
@@ -12,14 +12,10 @@ marker 'recipe_start_rightscale' do
   template 'rightscale_audit_entry.erb'
 end
 
-ruby_block 'deserialize byocm-chef attributes' do
-  block do
-    if ::File.exist?('/opt/byocm-chef/node.js')
-      ::File.open('/opt/byocm-chef/node.js', 'r') do |f|
-        vars = JSON.load(f)
-        node.override['rs-base']['collectd_server'] = vars['rs_sketchy']
-        node.override['collectd']['fqdn'] = vars['rs_instance_uuid']
-      end
-    end
+if ::File.exist?('/opt/byocm-chef/node.js')
+  ::File.open('/opt/byocm-chef/node.js', 'r') do |f|
+    vars = JSON.load(f)
+    node.override['rs-base']['collectd_server'] = vars['rs_sketchy']
+    node.override['collectd']['fqdn'] = vars['rs_instance_uuid']
   end
 end
